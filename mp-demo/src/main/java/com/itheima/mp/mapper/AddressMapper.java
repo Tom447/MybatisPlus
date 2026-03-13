d0aeb9e0afa4631e794ecdfbc36a00d3b504f102
xiaoma <1142322503@qq.com>
2026-03-13 18:23:12 +0800

    mybatisplus 自动生成代码

---
+++ mp-demo/src/main/java/com/itheima/mp/mapper/AddressMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.mapper;
+
+import com.itheima.mp.domain.po.Address;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 小马
+ * @since 2026-03-13
+ */
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
